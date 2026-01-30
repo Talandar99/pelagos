ce83836232f8c48a5d0fab50b2350fcf629f7a1a
Talandar <talandar66@protonmail.com>
2026-01-30 14:52:48 +0100

    resource fix

--- prototypes/planet/planet-resources.lua
+++ prototypes/planet/planet-resources.lua
@@ -74,7 +74,7 @@ data:extend({
 			},
 		},
 		map_color = { 0.0, 1.0, 0.0 },
-		map_grid = true,
+		map_grid = false,
 	},
 })
 data:extend({
@@ -156,7 +156,7 @@ data:extend({
 			},
 		},
 		map_color = { r = 1.0, g = 0.7, b = 0.0 },
-		map_grid = true,
+		map_grid = false,
 	},
 })
 --methane
